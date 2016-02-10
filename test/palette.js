d75219067791de632cbd476b01adee6e17fa93c3
Markus Ekholm <markus@botten.org>
2016-02-10 09:30:51 +0100

    Fixing license text typo

--- test/palette.js
+++ test/palette.js
@@ -1,7 +1,7 @@
 /**
  * @author Markus Ekholm
- * @copyright 2012-2015 (c) Markus Ekholm <markus at botten dot org >
- * @license Copyright (c) 2012, Markus Ekholm
+ * @copyright 2012-2016 (c) Markus Ekholm <markus at botten dot org >
+ * @license Copyright (c) 2012-2016, Markus Ekholm
  * All rights reserved.
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -10,7 +10,7 @@
  *    * Redistributions in binary form must reproduce the above copyright
  *      notice, this list of conditions and the following disclaimer in the
  *      documentation and/or other materials provided with the distribution.
- *    * Neither the name of the <organization> nor the
+ *    * Neither the name of the author nor the
  *      names of its contributors may be used to endorse or promote products
  *      derived from this software without specific prior written permission.
  *
